9bbcf2c384a25b49b2671bb290b818a7538a103c
Ramiro Monroy Ramos <ramiro.monroy@udea.edu.co>
2020-05-12 21:32:12 -0500

    Creating the service

---
+++ src/main/java/co/com/udea/service/CustomerService.java
@@ -0,0 +1,16 @@
+package co.com.udea.service;
+
+
+import java.util.List;
+
+import co.com.udea.entity.Customer;
+
+public class CustomerService {
+	
+public Customer save(Customer customer);
+	
+	public List<Customer> findAll();
+	
+	public Customer findById(Integer id);
+	
+}
