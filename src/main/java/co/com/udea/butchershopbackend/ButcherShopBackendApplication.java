d11e39dd9505a89b7c1b25337cae06f50a049551
Ramiro Monroy Ramos <ramiro.monroy@udea.edu.co>
2020-05-12 20:43:04 -0500

    Project Upload

---
+++ src/main/java/co/com/udea/butchershopbackend/ButcherShopBackendApplication.java
@@ -0,0 +1,13 @@
+package co.com.udea.butchershopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcherShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButcherShopBackendApplication.class, args);
+	}
+
+}
